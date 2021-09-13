fdd7f5cb70678d9a4639bcf669168fba238811ac
unknown <aribab98@gmail.com>
2021-09-14 00:01:18 +0700

    feat: add docker

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./router/vaccination_status_reports_router');
+const routes = require('./src/router/vaccination_status_reports_router');
 
 const app = express();
 const port = process.env.PORT;
