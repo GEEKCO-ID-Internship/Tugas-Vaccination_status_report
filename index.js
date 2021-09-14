5360fc43476a03e18048b2b22b7e8585b162d178
unknown <aribab98@gmail.com>
2021-09-14 18:27:03 +0700

    patch: fix cors problem

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const cors = require('cors')
+
 const routes = require('./src/router/vaccination_status_reports_router');
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors({origin: ['*']}));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, PATCH, DELETE');
+    next();
+});
 
 app.use(express.json());
 app.use('/api', routes);
